d42914e176c6b4c4459905dd23740b012b2110ce
Kiruthya Ramesh <46788862+Phoenix-RK@users.noreply.github.com>
2020-04-02 00:11:03 +0530

    Add files via upload

---
+++ Linked List/Remove duplicates in sorted LL.cpp
@@ -0,0 +1,99 @@
+//Phoenix_RK
+/*
+Given a singly linked list consisting of N nodes. The task is to remove duplicates (nodes with duplicate values) from the given list (if exists).
+Note: Try not to use extra space. Expected time complexity is O(N). The nodes are arranged in a sorted way.
+
+Input:
+2
+4
+2 2 4 5
+5
+2 2 2 2 2
+
+Output:
+2 4 5
+2
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+                Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// root: head node
+Node *removeDuplicates(Node *root)
+{
+ // your code goes here
+ Node *head=root;
+ while(head->next!=NULL)
+ {
+     if (head->data==head->next->data)
+     {
+         head->next=head->next->next;
+     }
+     else
+     {
+         head=head->next;
+     }
+     
+ }
+ return root;
+}
