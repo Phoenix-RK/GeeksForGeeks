99e2dcd24d94b0d14b86c9ab997bd8349c9036c4
Kiruthya Ramesh <kiruthyasp31@gmail.com>
2020-05-08 17:39:54 +0530

    Maximum difference between node and its ancestor

---
+++ Binary tree/Maximum difference between node and its ancestor.cpp
@@ -0,0 +1,158 @@
+/*
+
+Given a Binary Tree, you need to find the maximum value which you can get by subtracting the value of node B from the value of node A, where A and B are two nodes of the binary tree and A is an ancestor of B. You are required to complete the function maxDiff. You should not read any input from stdin/console. There are multiple test cases. For each test case, this method will be called individually.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+int maxDiff(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        
+        cout<<maxDiff(root)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+int finddiff(Node *root,int &diff)
+{
+    if(root==NULL)
+    return  INT_MAX;
+    
+    if(root->left==NULL && root->right==NULL)
+    return root->data;
+    
+   
+    
+   int l= finddiff(root->left,diff);
+   int r= finddiff(root->right,diff);
+    int val=min(l,r);
+    diff = max(diff, (root->data)-val);
+    return min(root->data,val);
+}
+
+/* Your are required to 
+complete this method*/
+int maxDiff(Node* root)
+{
+    // Your code here 
+    int diff=INT_MIN;
+    finddiff(root,diff);
+    return diff;
+    
+}
+
