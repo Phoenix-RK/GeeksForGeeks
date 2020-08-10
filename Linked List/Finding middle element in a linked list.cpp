a04b39d4f32378a05e14dc7c49980e29789eee63
Kiruthya Ramesh <kiruthyasp31@gmail.com>
2020-08-10 13:51:36 +0530

    Finding middle element in a linked list

---
+++ Linked List/Finding middle element in a linked list.cpp
@@ -0,0 +1,124 @@
+//Phoenix_RK
+
+
+/*
+https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+
+Given a singly linked list of N nodes. The task is to find the middle of the linked list. For example, if given linked list is 1->2->3->4->5 then the output should be 3.
+If there are even nodes, then there would be two middle nodes, we need to print the second middle element. For example, if given linked list is 1->2->3->4->5->6 then the output should be 4.
+
+Input:
+The first line of input contains the number of test cases T. For each test case, the first line of input contains the length of the linked list and the next line contains data of nodes of the linked list.
+
+Output:
+For each test case, there will be a single line of output containing data of the middle element of the linked list.
+
+User Task:
+The task is to complete the function getMiddle() which takes a head reference as the only argument and should return the data at the middle node of the linked list.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= T <= 500
+1 <= N <= 5000
+
+Example:
+Input:
+2
+5
+1 2 3 4 5
+6
+2 4 6 7 5 1
+
+Output:
+3
+7
+
+Explanation:
+Testcase 1: Since there are 5 elements, therefore 3 is the middle element at index 2 (0-based indexing).
+
+****************************
+Tortoise Hare method
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+/* Function to get the middle of the linked list*/
+int getMiddle(Node *head);
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		cout<<getMiddle(head)<<endl;
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   // Your code here
+   if(head==NULL)
+    return -1;
+   Node* slow=head,*fast=head;
+   
+   while(fast!=NULL && fast->next!=NULL)
+   {
+       slow=slow->next;
+       fast=fast->next->next;
+   }
+   
+   return slow->data;
+}
+
