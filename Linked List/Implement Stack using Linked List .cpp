c2c50508764c929c9e35581c3f6e9fd991681de6
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-08-23 16:14:50 +0530

    Implement Stack using Linked List

---
+++ Linked List/Implement Stack using Linked List .cpp
@@ -0,0 +1,86 @@
+//Phoenix_RK
+
+
+
+/*
+https://practice.geeksforgeeks.org/problems/implement-stack-using-linked-list/1
+
+Let's give it a try! You have a linked list and you have to implement the functionalities push and pop of stack using this given linked list. Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack. 
+
+Example 1:
+
+Input: 
+push(2)
+push(3)
+pop()
+push(4) 
+pop()
+Output: 3, 4
+Explanation: 
+push(2)    the stack will be {2}
+push(3)    the stack will be {2 3}
+pop()      poped element will be 3,
+           the stack will be {2}
+push(4)    the stack will be {2 4}
+pop()      poped element will be 4
+Example 2:
+
+Input: 
+pop()
+push(4)
+push(5)
+pop()
+Output: -1, 5
+Your Task: You are required to complete two methods push() and pop(). The push() method takes one argument, an integer 'x' to be pushed into the stack and pop() which returns an integer present at the top and popped out from the stack. If the stack is empty then return -1 from the pop() method.
+
+*/
+
+
+
+/*
+The structure of the node of the stack is
+struct StackNode
+{
+    int data;
+    StackNode *next;
+    StackNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+// And this is structure of MyStack
+class MyStack {
+private:
+StackNode *top;
+public :
+    void push(int);
+    int pop();
+    MyStack()
+    {
+        top = NULL;
+    }
+};
+
+/* The method push to push element
+   into the stack */
+void MyStack ::push(int x) {
+    // Your Code
+    StackNode* temp=new StackNode(x);
+    temp->next=top;
+    top=temp;
+}
+
+/* The method pop which return the element
+  poped out of the stack*/
+int MyStack ::pop() {
+    // Your Code
+    if(top==NULL)
+     return -1;
+    
+    int x=top->data;
+    top=top->next;
+    return x;
+    
+}
