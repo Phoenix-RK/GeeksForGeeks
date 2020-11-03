096e2b99618d87d3319a1adcecc8af4751000e80
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-11-03 19:52:25 +0530

    Reverse Level Order Traversal

---
+++ Binary tree/Reverse Level Order Traversal.cpp
@@ -0,0 +1,195 @@
+//Phoenix_RK
+
+
+/*
+https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1#
+
+
+Given a binary tree of size N, find its reverse level order traversal. ie- the traversal must begin from the last level.
+
+Example 1:
+
+Input :
+        1
+      /   \
+     3     2
+
+Output: 3 2 1
+Explanation:
+Traversing level 1 : 3 2
+Traversing level 0 : 1
+Example 2:
+
+Input :
+       10
+      /  \
+     20   30
+    / \ 
+   40  60
+
+Output: 40 60 20 30 10
+Explanation:
+Traversing level 2 : 40 60
+Traversing level 1 : 20 30
+Traversing level 0 : 10
+
+Your Task: 
+You dont need to read input or print anything. Complete the function reverseLevelOrder() which takes the root of the tree as input parameter and returns a list containing the reverse level order traversal of the given tree.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+vector<int> reverseLevelOrder(Node* root);
+
+int main()
+{
+
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        vector<int> result = reverseLevelOrder(root);
+        for (int i = 0; i < result.size(); ++i)
+        {
+            cout<<result[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+/*   
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+int height(Node* root)
+{
+    if(root==NULL)
+        return 0;
+        
+    return max(height(root->left),height(root->right))+1;
+}
+
+void getlevelval(Node* root,int l, vector<int>& reverse)
+{
+    if(root==NULL)
+        return;
+    if(l==0)
+        reverse.push_back(root->data);
+        
+    else
+    {
+        getlevelval(root->left,l-1,reverse);
+        getlevelval(root->right,l-1,reverse);
+    }
+    
+}
+
+vector<int> reverseLevelOrder(Node *root)
+{
+    // code here
+    vector<int> reverse;
+    int h=height(root); 
+    
+    for(int i=h;i>=0;i--)
+    {
+        getlevelval(root,i,reverse);
+    }
+    
+    return reverse;
+}
