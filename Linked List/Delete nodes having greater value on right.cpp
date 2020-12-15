effa703fddf40958b8e5d7525f7a6a28a071c9f9
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-12-15 17:01:48 +0530

    Delete nodes having greater value on right

---
+++ Linked List/Delete nodes having greater value on right.cpp
@@ -0,0 +1,43 @@
+//Phoenix_RK
+
+
+/*
+
+https://practice.geeksforgeeks.org/problems/delete-nodes-having-greater-value-on-right/1#
+
+
+
+Given a singly linked list, remove all the nodes which have a greater value on its next adjacent element.
+
+Example 1:
+
+Input:
+LinkedList = 12->15->10->11->5->6->2->3
+Output: 15 11 6 3
+Explanation: Since, 12, 10, 5 and 2 are
+the elements which have greater elements
+on their next node. So, after deleting
+them, the linked list would like be 15,
+11, 6, 3.
+Example 2:
+
+Input:
+LinkedList = 10->20->30->40->50->60
+Output: 60
+
+5
+5 4 3 2 6
+ans: 6
+
+
+Your Task:
+The task is to complete the function compute() which should modify the list as required and return the head of the modified linked list. The printing is done by the driver code,
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+
+
+
+*/
