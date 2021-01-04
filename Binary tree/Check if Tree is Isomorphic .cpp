3594dab0fecff07d07ab8c13e4f03489b3667a1a
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2021-01-04 16:39:22 +0530

    Check if Tree is Isomorphic

---
+++ Binary tree/Check if Tree is Isomorphic .cpp
@@ -0,0 +1,47 @@
+//Phoenix_RK
+
+
+/*
+
+https://practice.geeksforgeeks.org/problems/check-if-tree-is-isomorphic/1#
+
+Given two Binary Trees. Check whether they are Isomorphic or not.
+
+Example 1:
+
+Input:
+ T1    1     T2:   1
+     /   \        /  \
+    2     3      3    2
+   /            /
+  4            4
+Output: No
+
+Example 2:
+
+Input:
+T1    1     T2:    1
+    /  \         /   \
+   2    3       3     2
+  /                    \
+  4                     4
+Output: Yes
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isIsomorphic() that takes the root nodes of both the Binary Trees as its input and returns True if the two trees are isomorphic. Else, it returns False. (The driver code will print Yes if th returned values is true, otherwise false.)
+
+Note: 
+Two trees are called isomorphic if one of them can be obtained from other by a series of flips, i.e. by swapping left and right children of a number of nodes. Any number of nodes at any level can have their children swapped. Two empty trees are isomorphic.
+For example, following two trees are isomorphic with following sub-trees flipped: 2 and 3, NULL and 6, 7 and 8.
+ISomorphicTrees
+
+Expected Time Complexity: O(min(M,N)) where M and N are the sizes of the two trees.
+Expected Auxiliary Space: O(min(H1,H2)) where H1 and H2 are the heights of the two trees.
+
+Constraints:
+1<=Number of nodes<=105
+
+
+
+
+
+*/
